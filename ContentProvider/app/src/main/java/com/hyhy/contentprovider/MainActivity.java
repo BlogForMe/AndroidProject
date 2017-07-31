4e4cc75b7ce2aa7a07c683ab8fecc921d0e9377d
Jon <huaiyiheyuan@gmail.com>
2017-07-31 18:21:07 +0800

    ContentProvider提交

--- ContentProvider/app/src/main/java/com/hyhy/contentprovider/MainActivity.java
+++ ContentProvider/app/src/main/java/com/hyhy/contentprovider/MainActivity.java
@@ -1,15 +1,28 @@
 package com.hyhy.contentprovider;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String AUTHORITY = "com.hyhy.contenttest.db.FeedProvider";
+    private static final Uri ENTRY_ALL_URI = Uri.parse("content://" + AUTHORITY + "/entry");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btQuery(View v) {
+        Cursor c = getContentResolver().query(ENTRY_ALL_URI, null, null, null, null);
+        while (c.moveToNext()) {
+            System.out.println(c.getString(c.getColumnIndex("title")));
+        }
+    }
+
+
 }
--- ContentProvider/contenttest/src/main/java/com/hyhy/contenttest/MainActivity.java
+++ ContentProvider/contenttest/src/main/java/com/hyhy/contenttest/MainActivity.java
@@ -2,13 +2,13 @@ package com.hyhy.contenttest;
 
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.hyhy.contenttest.db.FeedReaderContract;
-import com.hyhy.contenttest.db.FeedReaderDbHelper;
+import com.hyhy.contenttest.db.ArticleDbHelper;
+import com.hyhy.contenttest.db.ArticleReaderContract;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(this);
+        ArticleDbHelper mDbHelper = new ArticleDbHelper(this);
         db = mDbHelper.getWritableDatabase();
     }
 
@@ -27,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
         ContentValues values;
         for (int i = 1; i < 5; i++) {
             values = new ContentValues();
-            values.put(FeedReaderContract.FeedEntry.COULUMN_NAME_ENTRY_ID, i);
-            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITILE, "heh " + i);
-            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, "describe " + i);
-            long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, FeedReaderContract.FeedEntry.TABLE_NAME, values);
+            values.put(ArticleReaderContract.Articles.COULUMN_NAME_ENTRY_ID, i);
+            values.put(ArticleReaderContract.Articles.COLUMN_NAME_TITILE, "heh " + i);
+            values.put(ArticleReaderContract.Articles.COLUMN_NAME_SUBTITLE, "describe " + i);
+            long newRowId = db.insert(ArticleReaderContract.Articles.TABLE_NAME, ArticleReaderContract.Articles.TABLE_NAME, values);
             if (newRowId != -1)
                 Toast.makeText(MainActivity.this, "插入成功", Toast.LENGTH_SHORT).show();
         }
